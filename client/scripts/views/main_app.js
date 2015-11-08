9277bb324590e9c932fc75c63ca81caee5b450b3
dgaspari <daniel.gaspari@gmail.com>
2015-11-07 23:06:55 -0500

    Updated app view and js to have a record button that presents pitch graph

--- client/scripts/views/main_app.js
+++ client/scripts/views/main_app.js
@@ -1,20 +1,113 @@
 'use strict';
 
+var Recorder = require('../app/recorder/recorder.js')
+
 var MainAppView = Backbone.View.extend({
 
   el: '#app-wrapper',
 
   template: JST['client/templates/main_app'],
 
-  events: {},
+  events: {
+    'click .record-audio': 'recordAudio',
+    'click .load-next-word': 'loadNextWord',
+    'click .load-prev-word': 'loadPrevWord'
+  },
 
   initialize: function() {
     this.render();
+    this.obtainMediaInfo();
   },
 
   render: function() {
     this.$el.html(this.template);
     return this;
+  },
+
+  obtainMediaInfo: function() {
+    var self = this;
+    var startMediaFunction = function(stream) { return self.startUserMedia(self, stream); };
+    //webkit shim
+    window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    window.URL = window.URL || window.webkitURL || window.mozURL;
+    navigator.getUserMedia({audio: true}, startMediaFunction, function(e) {
+      console.warn('No live audio input: ' + e);
+    });
+  },
+               
+  startUserMedia: function(self, stream) {
+    self.audio_context = new AudioContext();
+    var input = self.audio_context.createMediaStreamSource(stream);
+    console.log('Media stream created.');
+    self.volume = self.audio_context.createGain();
+    self.volume.gain.value = 0;
+    input.connect(self.volume)
+    self.volume.connect(self.audio_context.destination);
+    console.log('Input connected to audio context destination.');
+    self.recorder = new Recorder(input);
+    console.log('Recorder initialised.');
+  },
+
+  startRecording: function() {
+    console.log('Recording...');
+    this.recorder && this.recorder.record();
+  },
+
+  stopRecording: function() {
+    console.log('Stopped recording');
+    this.recorder && this.recorder.stop();
+    this.recorder && this.recorder.exportWAV(this.handleWAV);
+    this.recorder && this.recorder.clear();
+  },
+
+  //TODO: change testcontroller called here to something more appropriate:
+  handleWAV: function(blobdata) {
+    console.log('passing blob to python module...');
+    var uploadData = new FormData();
+    uploadData.append('audiodata', blobdata);
+    $.ajax({
+      url: 'test/sendfreq',
+      type: 'POST',
+      data: uploadData,
+      contentType: false,
+      processData: false,
+      success: function(results) {
+        console.log('rpc call returned:');
+        results.freqmap.unshift('voice frequency (hz)');
+        console.log(results.freqmap);
+        var chart = c3.generate({
+          bindto: '.audioChart',
+          data: {
+            columns: [
+              results.freqmap
+            ]
+          }
+        });
+      }
+    });
+  },
+
+  recordAudio: function(e) {
+    e.preventDefault();
+    var self = this;
+    //wait 1 second, start recording, stop after 3 seconds. load data
+    setTimeout(function() {
+      self.startRecording();
+      setTimeout(function() {
+        self.stopRecording();
+      }, 3000);
+    }, 1000);
+  },
+
+  loadNextWord: function(e) {
+    e.preventDefault(); 
+    alert('loading next word!');
+  },
+
+  loadPrevWord: function(e) {
+    e.preventDefault(); 
+    alert('loading previous word!');
   }
 
 });
